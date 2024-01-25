26faf792f0839816d7ba27eef7ffae2c1938e4f0
Web2Bariflo <web2.bariflo@outlook.com>
2024-01-25 20:20:38 +0530

    jan-25-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -69,6 +69,7 @@ const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'dashboard',component:DashboardComponent, canActivate:[AuthGuard]
 },
+{path:'verify-token', component:TokenVerifyComponent},
   {
     path: 'dashboard-dialog',
     loadChildren: () => import('./dashboard/dashboard-dialog.module').then(m => m.DashboardDialogModule),
